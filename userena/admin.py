60813f5c62f2de3c66f58be8f5e8cd127dbf5588
Bill Jones <bill@averline.com>
2011-03-16 15:02:16 -0400

    added admin function to send activation mails for deferred signups

--- userena/admin.py
+++ userena/admin.py
@@ -6,14 +6,25 @@ from django.utils.translation import ugettext as _
 from userena.models import UserenaSignup
 from userena.utils import get_profile_model
 
+    
 class UserenaSignupInline(admin.StackedInline):
     model = UserenaSignup
     max_num = 1
 
 class UserenaAdmin(UserAdmin):
+    def send_activation_email(self, request, queryset):
+        "send initial activation mails for (manually) validated new users"
+        for u in queryset:            
+            u.userena_signup.send_activation_email()
+            self.message_user(request, "Activation mail sent to %s" % u)
+    send_activation_email.short_description = \
+        "Send activation (approval) emails."
+    
     inlines = [UserenaSignupInline, ]
     list_display = ('username', 'email', 'first_name', 'last_name',
                     'is_staff', 'date_joined')
+    actions = [ 'send_activation_email', ]
+     
 
 admin.site.unregister(User)
 admin.site.register(User, UserenaAdmin)
